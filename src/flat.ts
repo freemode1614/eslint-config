6a58914fa1767b308c52622a79ce0be388207ff8
freemode <freemode1614@outlook.com>
2024-05-25 00:20:53 +0800

    chore: U

--- src/flat.ts
+++ src/flat.ts
@@ -1,3 +1,7 @@
 import base from "@/flat/base";
+import jest from "@/flat/jest";
+import json from "@/flat/json";
+import react from "@/flat/react";
+import typescript from "@/flat/typescript";
 
-export default [base];
+export default [...base, ...jest, ...json, ...react, ...typescript];
--- src/legacy.ts
+++ src/legacy.ts
@@ -1,65 +1,12 @@
-import { resolve } from "node:path";
+import { Linter } from "eslint";
 
-import { readJSONSync } from "fs-extra";
+import base from "@/legacy/base";
+import jest from "@/legacy/jest";
+import json from "@/legacy/json";
+import react from "@/legacy/react";
+import typescript from "@/legacy/typescript";
 
-import { legacy as baseConfigGen } from "@/base.js";
-import { legacy as jsonConfigGen } from "@/overrides/json.js";
-import { legacy as reactConfigGen } from "@/overrides/react.js";
-import { legacy as testConfigGen } from "@/overrides/test";
-import { legacy as tsConfigGen } from "@/overrides/typescript.js";
-
-/**
- */
-const package_ = readJSONSync(resolve(process.cwd(), "package.json"), {
-  throws: false,
-});
-
-if (!package_) {
-  throw new Error(
-    "No `package.json` found in local, make sure you using eslint in a valid nodejs package which include a `package.json` file.",
-  );
-}
-
-const {
-  dependencies = {}, //
-  devDependencies = {},
-  peerDependencies = {},
-} = package_;
-
-const localProjectDeps = Object.keys(Object.assign({}, dependencies, devDependencies, peerDependencies));
-
-const isUsingReact = localProjectDeps.includes("react");
-const isUsingPrettier = localProjectDeps.includes("prettier");
-const isUsingTypescript = localProjectDeps.includes("typescript");
-const isUsingJest = localProjectDeps.includes("jest");
-
-const isESModule = package_.type === "module";
-
-/**
- */
-const overrides = [
-  jsonConfigGen(), //
-  reactConfigGen({ isUsingTypescript }),
-  testConfigGen({ isUsingJest }),
-];
-
-if (isUsingTypescript) {
-  overrides.push(tsConfigGen());
-}
-
-const config = baseConfigGen({
-  isESModule,
-  isUsingPrettier,
-  isUsingReact,
-  isUsingTypescript,
-  extraConfig: {
-    overrides,
-  },
-});
-
-if (!process.env.DEBUGGER_LOG_MOCCONA_ESLINT_CONFIG) {
-  console.log(`config ->`, config);
-  process.env.DEBUGGER_LOG_MOCCONA_ESLINT_CONFIG = "LOGGED";
-}
-
-export default config;
+export default {
+  ...base,
+  overrides: [json, jest, react, typescript],
+} as Linter.Config;
