968eda8bd6f509a616f09c203665844db96834a3
wp.l <wenpeng.lei@ncsi.com.cn>
2024-07-18 17:59:17 +0800

    chore: update

--- src/flat/json.ts
+++ src/flat/json.ts
@@ -2,6 +2,7 @@ import { Linter } from "eslint";
 import jsonc from "eslint-plugin-jsonc";
 
 export default [
-  ...jsonc.configs["flat/recommended-with-json"], //
-  ...jsonc.configs["flat/prettier"]
+  ...jsonc.configs["flat/base"],
+  ...jsonc.configs["flat/prettier"],
+  ...jsonc.configs["flat/recommended-with-json"] //
 ] as Linter.FlatConfig[];
